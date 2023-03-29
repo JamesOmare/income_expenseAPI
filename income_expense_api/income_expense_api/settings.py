4487723dd147061cf2d7da8e98499a45a629de9d
JamesOmare <jamesomare922@gmail.com>
2023-03-29 09:58:45 +0300

    ...

--- income_expense_api/income_expense_api/settings.py
+++ income_expense_api/income_expense_api/settings.py
@@ -136,6 +136,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_POSR=587
+EMAIL_PORT=587
 EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD')
